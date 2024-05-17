94b7172887debfee117ba97c233e179fe4ca542f
Violet Sirikajornkij <thitirat.sirikajornkij@kellogg.northwestern.edu>
2024-05-17 19:37:48 +0000

    1st hr

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,15 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+  secret = "puppies"
+  encrypted_secret = BCrypt::Password.create(secret)
+  puts encrypted_secret
 # 2. prepare encrypted string for testing
+  user_entered_password = "puppies"
 # 3. test secret against prepared encrypted string
+  if BCrypt::Password.new(encrypted_secret) == user_entered_password  
+      valid = true
+  else
+      valid = false
+  end
+  puts valid
